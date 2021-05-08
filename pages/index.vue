5f2a894763c561b0531ddc7ee9bbe0331df35895
dotfeeb <83836988+dotfeeb@users.noreply.github.com>
2021-05-07 22:42:25 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -14,14 +14,14 @@
           target="_blank"
           class="button--green"
         >
-          Documentation
+          Twitter
         </a>
         <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://twitter.com/dotfeeb"
           target="_blank"
           class="button--grey"
         >
-          GitHub
+          Github
         </a>
       </div>
     </div>
@@ -29,9 +29,23 @@
 </template>
 
 <script>
+import Logo from '~/components/feeb (1).png'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
 
-<img src="~/components/feeblogo.png" width="500" height="600">
-
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -41,7 +55,6 @@
   color: #35495e;
   letter-spacing: 1px;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -49,7 +62,6 @@
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
 .links {
   padding-top: 15px;
 }
